ad0b464a82abbdc8a5917a1b729d37791d07f441
Maciej Węgrzyn <wegrzyn.maciej@o2.pl>
2020-04-19 18:19:04 +0200

    update 19.04.2020

--- scraper.py
+++ scraper.py
@@ -4,10 +4,43 @@ from bs4 import BeautifulSoup
 import pprint
 import json
 
+# funkcja do ekstrakcji składowych opinii
+
+
+def extract_feature(opinion, selector, attribute=None):
+    try:
+        if attribute:
+            return opinion.select(selector).pop()[attribute].strip()
+        else:
+            return opinion.select(selector).pop().text.strip()
+    except IndexError:
+        return None
+
+def replace_formatting(selector):
+    try:
+        return selector.replace("\n", ", ").replace("\r", ", ")
+    except AttributeError:
+        return selector
+
+# słownik z selektorami
+selectors = {
+    "author": ["div.reviewer-name-line"],
+    "recommendation": ["div.product-review-summary > em"],
+    "stars": ["span.review-score-count"],
+    "content": ["p.product-review-body"],
+    "cons": ["div.cons-cell > ul"],
+    "pros": ["div.pros-cell > ul"],
+    "useful": ["button.vote-yes > span"],
+    "useless": ["button.vote-no > span"],
+    "opinion_date": ["span.review-time > time:nth-child(1)", "datetime"],
+    "purchase_date": ["span.review-time > time:nth-child(2)", "datetime"]
+}
+
 # Adres URL pierwszej strony z opiniami o produkcie
 url_prefix = "https://www.ceneo.pl"
-url_postfix = "/85910996#tab=reviews"
-url = url_prefix + url_postfix
+product_id = input("Podaj identyfikator produktu: ")
+url_postfix = "#tab=reviews"
+url = url_prefix + "/" + product_id + url_postfix
 
 # Pusta lisa na opinie konsumentów
 all_opinions = []
@@ -16,52 +49,22 @@ while url:
 
     # Pobranie kodu pojedynczej strony z opiniami o produkcie
     response = requests.get(url)
-    page_dom = BeautifulSoup(response.text, 'html.parser')
+    page_dom = BeautifulSoup(response.text, "html.parser")
 
     # Wyciągnięcie z kodu strony fragmentów odpowiadających poszczególym opiniom
     opinions = page_dom.select("li.js_product-review")
 
     # Dla wszystkich opinii z danej strony wydobycie ich składowych
     for opinion in opinions:
-
-        opinion_id = opinion["data-entry-id"]
-        author = opinion.select("div.reviewer-name-line").pop().text.strip()
-        recommendation = opinion.select(
-            "div.product-review-summary > em").pop().text.strip()
-        stars = opinion.select("span.review-score-count").pop().text.strip()
-        content = opinion.select("p.product-review-body").pop().text.strip()
-        try:
-            cons = opinion.select("div.cons-cell > ul").pop().text.strip()
-        except IndexError:
-            cons = None
-        try:
-            pros = opinion.select("div.pros-cell > ul").pop().text.strip()
-        except IndexError:
-            pros = None
-
-        useful = opinion.select("button.vote-yes > span").pop().text.strip()
-        useless = opinion.select("button.vote-no > span").pop().text.strip()
-        opinion_date = opinion.select(
-            'span.review-time > time:nth-child(1)').pop()["datetime"].strip()
-        try:
-            purchase_date = opinion.select(
-                'span.review-time > time:nth-child(2)').pop()["datetime"].strip()
-        except IndexError:
-            purchase_date = None
-
-        features = {
-            "opinion_id": opinion_id,
-            "author": author,
-            "recommendation": recommendation,
-            "stars": stars,
-            "content": content,
-            "cons": cons,
-            "pros": pros,
-            "useful": useful,
-            "useless": useless,
-            "opinion_date": opinion_date,
-            "purchase_date": purchase_date
-        }
+        features = {key: extract_feature(opinion, *args)
+                    for key, args in selectors.items()}
+        features["opinion_id"] = int(opinion["data-entry-id"])
+        features["useful"] = int(features["useful"])
+        features["useless"] = int(features["useless"])
+        features["stars"] = float(features["stars"].split("/")[0].replace(",", "."))
+        features["content"] = features["content"].replace("\n", " ").replace("\n", " ")
+        features["pros"] = replace_formatting(features["pros"])
+        features["cons"] = replace_formatting(features["cons"])
         all_opinions.append(features)
     try:
         url = url_prefix + page_dom.select("a.pagination__next").pop()["href"]
@@ -70,7 +73,7 @@ while url:
     print(len(all_opinions))
     print(url)
 
-with open('opinions.json', 'w', encoding="UTF-8") as fp:
+with open("opinions_json/"+product_id+".json", "w", encoding="UTF-8") as fp:
     json.dump(all_opinions, fp, indent=4, ensure_ascii=False)
 
 # pprint.pprint(all_opinions)
