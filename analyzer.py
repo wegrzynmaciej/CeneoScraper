335b829037ba37b71b4bea16e48c22b620cd29a4
Maciej Węgrzyn <wegrzyn.maciej@o2.pl>
2020-05-16 18:19:06 +0200

    MW 2020-05-16

--- analyzer.py
+++ analyzer.py
@@ -15,10 +15,38 @@ product_id = input("Podaj identyfikator produktu: ")
 opinions = pd.read_json(input_directory + "/" + product_id + ".json")
 opinions = opinions.set_index("opinion_id")
 
+#podstatowe statystyki
 average_score = opinions.stars.mean().round(2)
 pros = opinions.pros.count()
 cons = opinions.cons.count()
+stars = opinions.stars.value_counts().sort_index().reindex(list(np.arange(0, 5.5, 0.5)), fill_value=0)
 
-stars = opinions.stars.value_counts()
+print(f'\
+Średnia ocena: {average_score}\n\
+Liczba opinii z zaletami: {pros}\n\
+Liczba opinii z wadami: {cons}\n\
+{stars} ')
 
-print(pros, cons, stars)
+#histogram częstości występowania poszczególnych ocen (gwiazdek)
+fig, ax = plt.subplots()
+stars.plot.bar(color="#f5c3c2")
+plt.title("****Gwiazdki****")
+plt.xlabel("Liczba gwiazdek")
+plt.ylabel("Liczba opinii")
+plt.xticks(rotation=0)
+#plt.show()
+plt.savefig("figures_png/"+product_id+"_bar.png")
+plt.close()
+
+#udział poszczególnych rekomendacji w ogólnej liczbie opinii
+recommendation = opinions.recommendation.value_counts()
+fig, ax = plt.subplots()
+recommendation.plot.pie(label="",autopct="%1.1f%%", colors=["#f5c3c2", "#89cff0"])
+plt.title("<<<<Rekomendacja>>>>")
+#plt.show()
+plt.savefig("figures_png/"+product_id+"_pie.png")
+plt.close()
+
+#opinions['purchased'] = False if opinions.purchase_date == None else True
+#print(opinions)
+#stars_purchased = pd.crosstab(opinions['stars'],opinions['purchased'])
